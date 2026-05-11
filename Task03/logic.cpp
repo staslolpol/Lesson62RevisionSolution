e29c1f19ac6e61cef09de9930f4f6b46e7634ff8
staslolpol <staspriho9@gmail.com>
2026-05-11 17:36:47 +0300

    tsk 03 complite

--- Task03/logic.cpp
+++ Task03/logic.cpp
@@ -8,6 +8,31 @@
 
 #include "logic.h"
 
+int find_rows_with_more_positive_values(int* row, int m) {
+	int pos_counter = 0;
+	int other_counter = 0;
+
+	for (int j = 0; j < m; j++) {
+		if (*(row + j) > 0) {
+			pos_counter++;
+		}
+		else {
+			other_counter++;
+		}
+	}
+
+	return pos_counter > other_counter ? 1 : 0;
+}
+
 int count_rows_with_more_positive_values(int** matrix, int n, int m) {
-	return 0;
+	if (n <= 0 || m <= 0 || matrix == nullptr) {
+		return 0;
+	}
+
+	int total_rows = 0;
+	for (int i = 0; i < n; i++) {
+		total_rows += find_rows_with_more_positive_values(*(matrix + i ), m);
+	}
+
+	return total_rows;
 }
