722698c4db58a97d359371aaf54dc374a784f5f4
staslolpol <staspriho9@gmail.com>
2026-05-11 18:42:37 +0300

    task 05 in progress

--- Task04/logic.cpp
+++ Task04/logic.cpp
@@ -8,6 +8,71 @@
 
 #include "logic.h"
 
+int find_max_in_columns_of_matrix(int** matrix, int n, int m) {
+	int max = -100;
+
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (*(*(matrix + j) + i) > max) {
+				max = *(*(matrix + j) + i);
+			}
+		}
+	}
+	return max;
+}
+
+int find_min_in_columns_of_matrix(int** matrix, int n, int m) {
+	int min = 100;
+
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (*(*(matrix + j) + i) < min) {
+				min = *(*(matrix + j) + i);
+			}
+		}
+	}
+	return min;
+}
+
+bool is_column_with_extreme_value(int** matrix, int n, int m, int max, int min) {
+
+
+	for (int j = 0; j < n; j++)
+	{
+		if (*(*(matrix + j) + m) == max || *(*(matrix + j) + m) == min) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
 int sum_elements_of_columns_with_extreme_value(int** matrix, int n, int m) {
-	return 0;
+	if (matrix == nullptr) {
+		return 0;
+	}
+	if (n <= 0 || m <= 0) {
+		return 0;
+	}
+
+	int sum = 0;
+
+	int max = find_max_in_columns_of_matrix(matrix, n, m);
+	int min = find_min_in_columns_of_matrix(matrix, n, m);
+
+	for (int i = 0; i < m; i++)
+	{
+		if (is_column_with_extreme_value(matrix, n, i, max, min)) {
+			for (int j = 0; j < n; j++)
+			{
+				sum += *(*(matrix + j) + i);
+			}
+		}
+	}
+
+	return sum;
 }
--- Task05/logic.cpp
+++ Task05/logic.cpp
@@ -8,4 +8,124 @@
 #include "logic.h"
 
 void get_last_local_minimum(int** matrix, int n, int m, int* ii, int* jj) {
+	if (n <= 0 || m <= 0 || matrix == nullptr || (n == 1 && m == 1)) {
+		return;
+	}
+
+	if (n > 1 && m > 1 && (*(*(matrix + n - 1) + m - 1) < *(*(matrix + n - 1) + m - 2)
+		&& *(*(matrix + n - 1) + m - 1) < *(*(matrix + n - 2) + m - 1)))
+	{
+		*ii = n;
+		*jj = m;
+		return;
+	}
+
+	if (n > 1 && m > 1) {
+		for (int j = m - 2; j > 1; j--) {
+			int element = *(*(matrix + n - 1) + j);
+			if (element < *(*(matrix + n - 1) + j - 1)
+				&& element < *(*(matrix + n - 1) + j + 1)
+				&& element < *(*(matrix + n - 2) + j)){
+				*ii = n;
+				*jj = j + 1;
+				return;
+			}
+		}
+	}
+
+	if (n > 1 && m > 1 && (*(*(matrix + n - 1) + 0) < *(*(matrix + n - 1) + 1)
+		&& *(*(matrix + n - 1) + 0) < *(*(matrix + n - 2) + 0)))
+	{
+		*ii = n;
+		*jj = 1;
+		return;
+	}
+
+	for (int i = n - 2; i > 1; i--)
+	{
+
+		for (int j = m - 2; j > 1; j--)
+		{
+			/*if (i == n - 1 && j == 0
+				&& *(*(matrix + i + 1) + j) > *(*(matrix + i) + j)
+				&& *(*(matrix + i) + j) > *(*(matrix + i) + j + 1))
+			{
+				*ii = i + 1;
+				*jj = j + 1;
+				break;
+			}
+			if (i == 0 && j == m - 1
+				&& *(*(matrix + i - 1) + j) > *(*(matrix + i) + j)
+				&& *(*(matrix + i) + j) > *(*(matrix + i) + j - 1))
+			{
+				*ii = i + 1;
+				*jj = j + 1;
+				break;
+			}
+
+			if (i == 0 && j == 0
+				&& *(*(matrix + i - 1) + j) > *(*(matrix + i) + j)
+				&& *(*(matrix + i) + j) > *(*(matrix + i) + j + 1))
+			{
+				*ii = i + 1;
+				*jj = j + 1;
+				break;
+			}
+			if (i > 0 && j == m - 1
+				&& *(*(matrix + i + 1) + j) > *(*(matrix + i) + j)
+				&& *(*(matrix + i) + j) > *(*(matrix + i) + j - 1)
+				&& *(*(matrix + i) + j) > *(*(matrix + i) + j + 1))
+			{
+				*ii = i + 1;
+				*jj = j + 1;
+				break;
+			}
+			if (i > 0 && j == 0
+				&& *(*(matrix + i - 1) + j) > *(*(matrix + i) + j)
+				&& *(*(matrix + i) + j) > *(*(matrix + i) + j - 1)
+				&& *(*(matrix + i) + j) > *(*(matrix + i) + j + 1))
+			{
+				*ii = i + 1;
+				*jj = j + 1;
+				break;
+
+			}
+
+			if (i == n - 1 && j > 0
+				&& *(*(matrix + i + 1) + j) > *(*(matrix + i) + j)
+				&& *(*(matrix + i - 1) + j) > *(*(matrix + i) + j)
+				&& *(*(matrix + i) + j) > *(*(matrix + i) + j - 1))
+			{
+				*ii = i + 1;
+				*jj = j + 1;
+				break;
+
+			}
+
+			if (i == 0 && j > 0
+				&& *(*(matrix + i + 1) + j) > *(*(matrix + i) + j)
+				&& *(*(matrix + i - 1) + j) > *(*(matrix + i) + j)
+				&& *(*(matrix + i) + j) > *(*(matrix + i) + j + 1)
+				)
+			{
+				*ii = i + 1;
+				*jj = j + 1;
+				break;
+
+			}*/
+			int element = *(*(matrix + i) + j);
+			if (*(*(matrix + i + 1) + j) > element
+				&& *(*(matrix + i - 1) + j) > element
+				&& *(*(matrix + i) + j + 1) > element
+				&& *(*(matrix + i) + j - 1) > element) {
+
+				*ii = i + 1;
+				*jj = j + 1;
+
+			}
+		}
+
+
+	}
+
 }
