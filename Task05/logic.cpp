b33dc60dad4246657edb571ff87eb40259405db1
staslolpol <staspriho9@gmail.com>
2026-05-11 18:46:23 +0300

    1

--- Task05/logic.cpp
+++ Task05/logic.cpp
@@ -41,78 +41,25 @@ void get_last_local_minimum(int** matrix, int n, int m, int* ii, int* jj) {
 		return;
 	}
 
+	if (n > 1 && m > 1) {
+		for (int i = n - 2; i > 1; i--) {
+			int element = *(*(matrix + n - 2) + m);
+			if (element < *(*(matrix + n - 1) + m - 1)
+				&& element < *(*(matrix + n - 1) + m + 1)
+				&& element < *(*(matrix + n - 2) + m)) {
+				*ii = n + 1;
+				*jj = m;
+				return;
+			}
+		}
+	}
+
 	for (int i = n - 2; i > 1; i--)
 	{
 
 		for (int j = m - 2; j > 1; j--)
 		{
-			/*if (i == n - 1 && j == 0
-				&& *(*(matrix + i + 1) + j) > *(*(matrix + i) + j)
-				&& *(*(matrix + i) + j) > *(*(matrix + i) + j + 1))
-			{
-				*ii = i + 1;
-				*jj = j + 1;
-				break;
-			}
-			if (i == 0 && j == m - 1
-				&& *(*(matrix + i - 1) + j) > *(*(matrix + i) + j)
-				&& *(*(matrix + i) + j) > *(*(matrix + i) + j - 1))
-			{
-				*ii = i + 1;
-				*jj = j + 1;
-				break;
-			}
-
-			if (i == 0 && j == 0
-				&& *(*(matrix + i - 1) + j) > *(*(matrix + i) + j)
-				&& *(*(matrix + i) + j) > *(*(matrix + i) + j + 1))
-			{
-				*ii = i + 1;
-				*jj = j + 1;
-				break;
-			}
-			if (i > 0 && j == m - 1
-				&& *(*(matrix + i + 1) + j) > *(*(matrix + i) + j)
-				&& *(*(matrix + i) + j) > *(*(matrix + i) + j - 1)
-				&& *(*(matrix + i) + j) > *(*(matrix + i) + j + 1))
-			{
-				*ii = i + 1;
-				*jj = j + 1;
-				break;
-			}
-			if (i > 0 && j == 0
-				&& *(*(matrix + i - 1) + j) > *(*(matrix + i) + j)
-				&& *(*(matrix + i) + j) > *(*(matrix + i) + j - 1)
-				&& *(*(matrix + i) + j) > *(*(matrix + i) + j + 1))
-			{
-				*ii = i + 1;
-				*jj = j + 1;
-				break;
-
-			}
-
-			if (i == n - 1 && j > 0
-				&& *(*(matrix + i + 1) + j) > *(*(matrix + i) + j)
-				&& *(*(matrix + i - 1) + j) > *(*(matrix + i) + j)
-				&& *(*(matrix + i) + j) > *(*(matrix + i) + j - 1))
-			{
-				*ii = i + 1;
-				*jj = j + 1;
-				break;
-
-			}
-
-			if (i == 0 && j > 0
-				&& *(*(matrix + i + 1) + j) > *(*(matrix + i) + j)
-				&& *(*(matrix + i - 1) + j) > *(*(matrix + i) + j)
-				&& *(*(matrix + i) + j) > *(*(matrix + i) + j + 1)
-				)
-			{
-				*ii = i + 1;
-				*jj = j + 1;
-				break;
-
-			}*/
+			
 			int element = *(*(matrix + i) + j);
 			if (*(*(matrix + i + 1) + j) > element
 				&& *(*(matrix + i - 1) + j) > element
